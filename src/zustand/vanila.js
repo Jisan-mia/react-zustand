a9a769dc2190b4b798a46bede77ae311519862c9
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2024-10-26 15:58:35 +0600

    format

--- src/zustand/vanila.js
+++ src/zustand/vanila.js
@@ -1,56 +1,55 @@
-
 import { createStore } from "zustand/vanilla";
 
 window.onload = () => {
-  document.querySelector('#root').innerHTML += `<div id="counterContainer">
+  document.querySelector("#root").innerHTML += `<div id="counterContainer">
       <h1 id="counter"></h1>
 
       <button id="incrementBtn">increment</button>
       <button id="decrementBtn">decrement</button>
       <button id="resetBtn">reset</button>
     </div>`;
-// select elements
-const $counter = document.getElementById("counter");
-const $incrementBtn = document.getElementById("incrementBtn");
-const $decrementBtn = document.getElementById("decrementBtn");
-const $resetBtn = document.getElementById("resetBtn");
+  // select elements
+  const $counter = document.getElementById("counter");
+  const $incrementBtn = document.getElementById("incrementBtn");
+  const $decrementBtn = document.getElementById("decrementBtn");
+  const $resetBtn = document.getElementById("resetBtn");
 
-const counterStore = createStore((set) => ({
-  counter: 0,
-  increment: () => set((state) => ({ counter: state.counter + 1 })),
-  decrement: () => set((state) => ({ counter: state.counter - 1 })),
-  reset: () => set({ counter: 0 }),
-}));
+  const counterStore = createStore((set) => ({
+    counter: 0,
+    increment: () => set((state) => ({ counter: state.counter + 1 })),
+    decrement: () => set((state) => ({ counter: state.counter - 1 })),
+    reset: () => set({ counter: 0 }),
+  }));
 
-function incrementCounter() {
-  counterStore.getState().increment();
-}
+  function incrementCounter() {
+    counterStore.getState().increment();
+  }
 
-function decrementCounter() {
-  counterStore.getState().decrement();
-}
+  function decrementCounter() {
+    counterStore.getState().decrement();
+  }
 
-function resetCounter() {
-  counterStore.getState().reset();
-}
+  function resetCounter() {
+    counterStore.getState().reset();
+  }
 
-$incrementBtn.addEventListener("click", () => {
-  incrementCounter();
-});
+  $incrementBtn.addEventListener("click", () => {
+    incrementCounter();
+  });
 
-$decrementBtn.addEventListener("click", () => {
-  decrementCounter();
-});
-$resetBtn.addEventListener("click", () => {
-  resetCounter();
-});
+  $decrementBtn.addEventListener("click", () => {
+    decrementCounter();
+  });
+  $resetBtn.addEventListener("click", () => {
+    resetCounter();
+  });
 
-const render = (state) => {
-  console.log(state)
-  $counter.innerHTML = state.counter;
+  const render = (state) => {
+    console.log(state);
+    $counter.innerHTML = state.counter;
+  };
+
+  render(counterStore.getInitialState(), counterStore.getInitialState());
+
+  counterStore.subscribe(render);
 };
-
-render(counterStore.getInitialState(), counterStore.getInitialState())
-
-counterStore.subscribe(render)
-}
